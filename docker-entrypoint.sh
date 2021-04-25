474e12ca7689cdd9d173fe925ead8b8c9e06a901
Mohamed Khalifa <khalifa.ah@gmail.com>
2021-04-25 10:00:02 +0200

    convert baseline to string

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,7 @@ if [[ "$IS_BASELINE" -eq 1 ]];
 then
 	if [ -z "$BASELINE_VERSION" ]; then 
 		echo "Baseline Version is NULL then Set it to 1";
-		$BASELINE_VERSION = 1
+		$BASELINE_VERSION = "1"
 	else 
 		echo "Baseline is $BASELINE_VERSION"; 
 	fi
